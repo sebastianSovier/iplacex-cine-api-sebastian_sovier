9e4160147b50d845af4b5f554974a132b587486a
sebastian sovier <sebastian.sovier@tgtgroup.cl>
2024-11-15 11:16:17 -0300

    ports and fix uri mongo

--- src/common/db.js
+++ src/common/db.js
@@ -1,6 +1,6 @@
 import {MongoClient,ServerApiVersion} from 'mongodb'
 
-const uri = "mongodb+srv://eva3_express:W7lu2oGvs0naytrb@cluster-express.tbpjk.mongodb.net/?retryWrites=true&w=majority&appName=cluster-express"
+const uri = "mongodb+srv://eva3_express:W7lu2oGvs0naytrb@cluster-express.tbpjk.mongodb.net/?retryWrites=true&w=majority&appName=cluster-express&tls=true&sslInvalidHostNameAllowed=true"
 const client = new MongoClient(uri,{
     serverApi:{
         version:ServerApiVersion.v1,
